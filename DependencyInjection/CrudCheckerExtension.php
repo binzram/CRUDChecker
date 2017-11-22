cbb18076a298540e5e4ce54ec9a70270379b4053
Ramon Binz <binzram@gmail.com>
2017-11-22 23:07:25 +0100

    edit path to service.yml

--- DependencyInjection/CrudCheckerExtension.php
+++ DependencyInjection/CrudCheckerExtension.php
@@ -17,7 +17,7 @@ class CrudCheckerExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../config'));
         $loader->load('services.yml');
 
         $configuration = new Configuration();
